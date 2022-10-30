8fcfd9a2d224e2190028e43d522c5ae93759ff17
Nick Zozor <nick.zozor@gmail.com>
2022-10-30 13:51:25 +0000

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "http://www.beautiskinclinic.com"],
+  })
+);
 
 app.get("/date", (req, res) => {
   res.send(new Date());
